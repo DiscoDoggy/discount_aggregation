ee285ef6e60733fdc37a7cd672bf2c7880ed6f59
DiscoDoggy <akaattarilim@gmail.com>
2024-06-06 12:26:44 -0700

    Started transforming data for UNIQLO

--- base_transformer.py
+++ base_transformer.py
@@ -1,4 +1,15 @@
+from staging_ingestor import *
+from abc import abstractmethod
 
 class BaseTransformer():
     def __init__(self, extraction_id:str):
+        self.ingestion_client = StagingIngestor().setup_client()
         self.extraction_id_to_transform = extraction_id
+
+    def get_data_to_transform(self):
+        return self.ingestion_client.extraction_results.find({"extraction_id": self.extraction_id})
+    
+    @abstractmethod
+    def transform_data(self):
+        pass
+    
--- uniqlo_transformer.py
+++ uniqlo_transformer.py
@@ -1,4 +1,27 @@
 from base_transformer import *
+import uuid
+from datetime import datetime
 
 class UniqloTransformer(BaseTransformer):
+    def __init__(self, extraction_id):
+        super().__init__(extraction_id=extraction_id)
+
+    def transform_data(self):
+        unprocessed_data = super().get_data_to_transform()
+        processed_data = []
+        for data_point in unprocessed_data:
+
+            #TODO: Truncate/pad to 2 decimal points
+            data_point['base_price'] = float(data_point['base_price'])
+            data_point['promo_price'] = float(data_point['promo_price'])
+            data_point['ratings'] = float(data_point['ratings'])
+
+            data_point['extraction_id'] = uuid.UUID(data_point['extraction_id'])
+            
+            #converting UNIX timestamp to date
+
+            #datetime is in current string format 2024-06-05 02:33:19.270915+00:00
+            data_point['extraction_datetime'] = datetime.strptime(data_point['extraction_datetime'], "%Y-%m-%d %H:%M:%S")            
+
+
     
